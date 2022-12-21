cdc2854cff801670d5ec134a2f3c85d3efb0d90a
vladfreishmidt <vladfreishmidt.webdevelopment@gmail.com>
2022-12-21 15:02:16 +0200

    Install fractional

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,4 +1,5 @@
 import icons from 'url:../../img/icons.svg';
+import { Fraction } from 'fractional';
 
 class RecipeView {
     #parentElement = document.querySelector('.recipe');
@@ -81,18 +82,7 @@ class RecipeView {
             <div class="recipe__ingredients">
                 <h2 class="heading--2">Recipe ingredients</h2>
                 <ul class="recipe__ingredient-list">
-                ${this.#data.ingredients.map(ingredient => `
-                <li class="recipe__ingredient">
-                    <svg class="recipe__icon">
-                    <use href="src/img/icons.svg#icon-check"></use>
-                    </svg>
-                    <div class="recipe__quantity">${ingredient.quantity ? ingredient.quantity : ''}</div>
-                    <div class="recipe__description">
-                    <span class="recipe__unit">${ingredient.unit}</span>
-                    ${ingredient.description}
-                    </div>
-                </li>
-                `).join('')}
+                ${this.#data.ingredients.map(this.#generateIngredientMarkup).join('')}
                 </ul>
             </div>
 
@@ -115,7 +105,28 @@ class RecipeView {
                 </a>
             </div>
         `;
-    } 
+    }
+    
+    #generateIngredientMarkup(ingredient) {
+        return `
+            <li class="recipe__ingredient">
+                <svg class="recipe__icon">
+                <use href="src/img/icons.svg#icon-check"></use>
+                </svg>
+                <div class="recipe__quantity">
+                    ${
+                        ingredient.quantity ? 
+                        new Fraction(ingredient.quantity).toString() : 
+                        ''
+                    }
+                </div>
+                <div class="recipe__description">
+                <span class="recipe__unit">${ingredient.unit}</span>
+                ${ingredient.description}
+                </div>
+            </li>
+        `
+    }
 }
 
 export default new RecipeView();
